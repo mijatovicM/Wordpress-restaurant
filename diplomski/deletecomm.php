c447b3a7d56aec6886aa9f019ea48c3f7e50b295
Milos Mijatovic <miilosmijatovic@gmail.com>
2019-08-22 13:43:20 +0200

    Removed

--- diplomski/deletecomm.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-include_once("config/dbconfig.php");
-global $connection;
-$select = "DELETE FROM comments WHERE comments_id='".$_GET['delcommm_id']."'";
-$query = mysqli_query($connection, $select) or die($select);
-header ("Location: commented.php");
-
-?>
